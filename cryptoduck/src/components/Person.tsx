c4366648951daf32e36c293bcefc9d20dec0ed3d
md201007 <md201007@fhstp.ac.at>
2021-12-01 15:44:51 +0100

    setup

---
+++ cryptoduck/src/components/Person.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface IPerson {
+    firstName: string;
+    secondName:string;
+}
+
+const Person = ({ firstName, secondName }: IPerson) => {
+    return (
+        <div>
+            {firstName} {secondName}
+        </div>
+    );
+};
+
+export default Person;
