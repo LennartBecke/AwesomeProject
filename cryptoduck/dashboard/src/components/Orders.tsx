a9ffa4a8471c6096b44123bde905432125c5b5a8
LennartBecke <94056489+LennartBecke@users.noreply.github.com>
2021-12-20 14:32:29 +0100

    marketplace rename

--- cryptoduck/dashboard/src/components/Orders.tsx
+++ cryptoduck/dashboard/src/components/Orders.tsx
@@ -69,7 +69,7 @@ function preventDefault(event: React.MouseEvent) {
 export default function Orders() {
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
+      <Title>Marketplaces</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
