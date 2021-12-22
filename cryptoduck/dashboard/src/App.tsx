3a8c24e9aece6a6102b208b22edd910e6373d42a
LennartBecke <94056489+LennartBecke@users.noreply.github.com>
2021-12-22 13:04:40 +0100

    question 01

--- cryptoduck/dashboard/src/App.tsx
+++ cryptoduck/dashboard/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Startpage from "./components/Startpage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return <Startpage />;
