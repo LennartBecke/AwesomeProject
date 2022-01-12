6bec868cae79a5c3a4ca6faf4b1929ce15d5c697
LennartBecke <94056489+LennartBecke@users.noreply.github.com>
2022-01-12 20:39:28 +0100

    Bitcoin Info untereinander

--- cryptoduck/dashboard/src/components/Bitcoin.tsx
+++ cryptoduck/dashboard/src/components/Bitcoin.tsx
@@ -170,6 +170,7 @@ function DashboardContent() {
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
               {/* Chart */}
+
               <Grid item xs={12} md={8} lg={9}>
                 <Paper
                   sx={{
@@ -182,26 +183,30 @@ function DashboardContent() {
                   <Chart />
                 </Paper>
               </Grid>
+
               {/* Bitcoin Info */}
-              <Grid item xs={12} md={4} lg={3}>
+              <Grid item xs={12} md={8} lg={9}>
                 <Paper
                   sx={{
                     p: 2,
                     display: "flex",
                     flexDirection: "column",
-                    height: 640,
+                    height: 440,
                   }}
                 >
                   <Bitcoin_Info />
                 </Paper>
               </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12} md={4} lg={6}>
+
+              {/* Recent Orders (Marketplaces) */}
+              <Grid item xs={12} md={8} lg={9}>
                 <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
                   <Orders />
                 </Paper>
               </Grid>
+
             </Grid>
+
             <Copyright sx={{ pt: 4 }} />
           </Container>
         </Box>
