88cc7954d171dae0682f58b5467631f59f2b6b7b
LennartBecke <94056489+LennartBecke@users.noreply.github.com>
2022-02-28 15:33:34 +0100

    ok

--- cryptoduck/dashboard/src/App.tsx
+++ cryptoduck/dashboard/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Startpage from "./components/Startpage";
+import Startpage from "./components/Startpage_02";
 
 function App() {
   return <Startpage />;
